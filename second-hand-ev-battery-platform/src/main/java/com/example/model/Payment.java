a280a763f83652fe10a16ab4ffab68c2e2c10470
DangQuangThang170605 <thangdq1083@ut.edu.vn>
2025-10-27 08:48:33 +0700

    Update Payment.java

--- second-hand-ev-battery-platform/src/main/java/com/example/model/Payment.java
+++ second-hand-ev-battery-platform/src/main/java/com/example/model/Payment.java
@@ -1,1 +1,24 @@
+package com.example.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@Entity
+@Table(name = "Payment")
+public class Payment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long paymentID;
+
+    private BigDecimal amount;
+    private String method;
+    private LocalDateTime paymentDate;
+
+    @ManyToOne
+    @JoinColumn(name = "userID")
+    private User user;
+}
 
