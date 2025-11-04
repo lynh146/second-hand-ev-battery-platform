36e31c97f743cd437e882677296ffc29cdb842c5
DangQuangThang170605 <thangdq1083@ut.edu.vn>
2025-11-04 18:43:38 +0700

    Update AdminRepository.java

--- src/main/java/com/example/repository/AdminRepository.java
+++ src/main/java/com/example/repository/AdminRepository.java
@@ -1,8 +1,10 @@
 package com.example.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.model.Admin;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface AdminRepository extends JpaRepository<Admin, Long> {
+    Admin findByEmailAndPassword(String email, String password);
 }
