1bce280ccacfb45a930c1481e295e88317ba0711
dat1606 <trandat0616@gmail.com>
2025-11-04 21:40:14 +0700

    Thêm các file repository: Listing,Vehicle.

---
+++ src/main/java/com/example/repository/VehicleRepository.java
@@ -0,0 +1,18 @@
+package com.example.repository;
+
+import com.example.model.Vehicle;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
+
+@Repository
+public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
+    
+    List<Vehicle> findByBrandContainingIgnoreCase(String brand);
+    
+    List<Vehicle> findByModelContainingIgnoreCase(String model);
+    
+    List<Vehicle> findByManufactureYear(int manufactureYear);
+    
+    List<Vehicle> findByStatus(String status);
+}
