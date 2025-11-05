adfc8686f1c081511cc309ff20b5cd79a435a886
lynh146 <nguyenthuylinhl680@gmail.com>
2025-11-05 21:30:37 +0700

    create IListingService

---
+++ src/main/java/com/example/service/IListingService.java
@@ -0,0 +1,11 @@
+package com.example.service;
+
+import java.util.List;
+import com.example.model.Admin;
+import com.example.model.Listing;
+
+public interface IListingService {
+    void createListing(Listing l);
+    void approveListing(Long id, Admin a);
+    List<Listing> searchListing(String keyword); 
+}
