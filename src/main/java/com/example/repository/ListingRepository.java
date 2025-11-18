d068c954bcd76ef8c7631314d393ed3f92b1e70d
DangQuangThang170605 <thangdq1083@ut.edu.vn>
2025-11-18 09:46:50 +0700

    Update ListingRepository.java

--- src/main/java/com/example/repository/ListingRepository.java
+++ src/main/java/com/example/repository/ListingRepository.java
@@ -1,8 +1,13 @@
 package com.example.repository;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 import com.example.model.Listing;
 
+@Repository
 public interface ListingRepository extends JpaRepository<Listing, Long> {
+
+    // Lấy danh sách tin theo status (pending, approved, rejected)
+    List<Listing> findByStatus(String status);
 }
