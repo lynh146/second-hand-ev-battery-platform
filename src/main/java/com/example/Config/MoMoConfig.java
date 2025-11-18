d3cc4baecdf44cdb357637726e7c49be5c88148c
BaoBao <nguyenhoangaz2005@gmail.com>
2025-11-18 22:21:18 +0700

    update Config

---
+++ src/main/java/com/example/Config/MoMoConfig.java
@@ -0,0 +1,20 @@
+package com.example.Config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;  
+import org.springframework.context.annotation.Configuration;
+import lombok.Data;
+
+@Configuration
+@Data
+@ConfigurationProperties(prefix = "momo")  
+public class MoMoConfig {
+
+    private String partnerCode; 
+    private String accessKey;
+    private String secretKey;
+    private String createPaymentUrl; 
+    private String redirectUrl;
+    private String ipnUrl;
+    
+    public static final String REQUEST_TYPE_ATM = "payWithATM"; 
+}
