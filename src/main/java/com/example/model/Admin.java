360667dbc70673dc65d708add1de1968416c8d4a
DangQuangThang170605 <thangdq1083@ut.edu.vn>
2025-11-20 23:40:52 +0700

    Update Admin.java

--- src/main/java/com/example/model/Admin.java
+++ src/main/java/com/example/model/Admin.java
@@ -1,28 +1,20 @@
 package com.example.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Table(name = "ADMIN")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "ADMIN") // trùng với sơ đồ bạn vẽ
 public class Admin {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "AdminID")   // giữ nguyên đúng như trong DB
-    private Long adminID; 
+    @Column(name = "AdminID")
+    private Long adminID;
 
     @Column(name = "FullName")
     private String fullName;
@@ -35,7 +27,6 @@ public class Admin {
 
     @Column(name = "PhoneNumber")
     private String phoneNumber;
-
 
     @Column(name = "PermissionLevel")
     private int permissionLevel;
