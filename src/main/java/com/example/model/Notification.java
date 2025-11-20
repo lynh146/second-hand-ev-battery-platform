4250f34876524ba52256f826a1f461fed0b02025
DangQuangThang170605 <thangdq1083@ut.edu.vn>
2025-11-20 18:32:34 +0700

    Update Notification.java

--- src/main/java/com/example/model/Notification.java
+++ src/main/java/com/example/model/Notification.java
@@ -1,49 +1,31 @@
 package com.example.model;
 
+import jakarta.persistence.*;
+import lombok.*;
 import java.time.LocalDateTime;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.Table;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
-@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "NOTIFICATION") 
+@Entity
+@Table(name = "NOTIFICATION")
 public class Notification {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "NotificationID") 
-    private Long notificationID;
+    private Long id;
 
-    @ManyToOne
-    @JoinColumn(name = "UserID")  
-    private User user;
-
-    @Column(name = "Title")
-    private String title;
-
-    @Column(name = "Message")
+    @Column(nullable = false)
     private String message;
 
-    @Column(name = "Type")
-    private String type;
+    @Column(nullable = false)
+    private boolean isRead;
 
-    @Column(name = "CreatedAt")
+    @Column(nullable = false)
     private LocalDateTime createdAt;
 
-    @Column(name = "IsRead")
-    private Boolean isRead;
+    @ManyToOne
+    @JoinColumn(name = "UserID", nullable = false)
+    private User user;
 }
