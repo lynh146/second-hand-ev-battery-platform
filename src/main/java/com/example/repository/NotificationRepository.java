981badd8cd1fca5ccee8ade1651df9f101ae34ac
DangQuangThang170605 <thangdq1083@ut.edu.vn>
2025-11-20 18:31:57 +0700

    Update NotificationRepository.java

--- src/main/java/com/example/repository/NotificationRepository.java
+++ src/main/java/com/example/repository/NotificationRepository.java
@@ -1,8 +1,13 @@
 package com.example.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.model.Notification;
+import com.example.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.List;
 
+@Repository
 public interface NotificationRepository extends JpaRepository<Notification, Long> {
+    List<Notification> findByUserOrderByCreatedAtDesc(User user);
+    List<Notification> findByUserAndIsReadFalse(User user);
 }
