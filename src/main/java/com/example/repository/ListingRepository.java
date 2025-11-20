79c0eff474b841ddd5ed5ec98539539e9bbccc0d
DangQuangThang170605 <thangdq1083@ut.edu.vn>
2025-11-21 01:00:33 +0700

    Update ListingRepository.java

--- src/main/java/com/example/repository/ListingRepository.java
+++ src/main/java/com/example/repository/ListingRepository.java
@@ -1,13 +1,31 @@
 package com.example.repository;
 
-import java.util.List;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import com.example.model.Listing;
+import com.example.model.Admin;
 
-@Repository
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+
+
 public interface ListingRepository extends JpaRepository<Listing, Long> {
-
-    // Lấy danh sách tin theo status (pending, approved, rejected)
     List<Listing> findByStatus(String status);
+
+    List<Listing> findByStatusAndTitleContainingIgnoreCase(String status, String keyword);
+
+    Page<Listing> findByStatus(String status, Pageable pageable);
+
+    Listing findByListingIDAndStatus(Long listingID, String status);
+
+    List<Listing> findByApprovedBy(Admin admin);
+
+    Page<Listing> findByApprovedTrueAndStatus(String status, org.springframework.data.domain.Pageable pageable);
+
+    List<Listing> findByStatusAndApprovedByIsNull(String status);
+
+    List<Listing> findByApprovedBy(Admin admin);
+
 }
+
